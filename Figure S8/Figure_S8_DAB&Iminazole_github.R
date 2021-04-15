8d0b05cc8843625c70cf5228de3b78d057daabc7
HaiyanXiong2009 <45755738+HaiyanXiong2009@users.noreply.github.com>
2021-04-15 06:50:37 +0100

    Add files via upload

---
+++ Figure S8/Figure_S8_DAB&Iminazole_github.R
@@ -0,0 +1,37 @@
+data<-read.csv("Green&Etiolated leaves_Imidazole_phenotype.csv")
+
+data$Type<-factor(data$Type,levels=c("Green","Etio"))
+data$treat2<-factor(data$treat2,levels=c("treat","ck"))
+data$group1<-factor(data$group1,levels=c("Green_DAB","Green_Imi","Etio_DAB","Etio_Imi"))
+
+data$time <- as.numeric(data$time)
+pd <- position_dodge(0.7)
+
+pdf("Rplot02_Green & Etiolated-Imi.pdf",width=10,height=3.5)
+ggplot(data, aes(x = time, y = value,group=group,color=group1))+
+  geom_errorbar(aes(ymin=value-se, ymax=value+se), width=4,
+                size=0.3,
+                position=pd) +
+  geom_line(aes(color=group1,linetype=treat2),alpha=1, size=0.7, stat = "identity")+
+  geom_point(position=pd, size=1, shape=21, fill="white")+
+  facet_wrap(~Type, ncol=2)+
+  scale_color_manual(values = c("#00ba38","blue","#00ba38","blue"))+
+  scale_fill_manual(values=c("#00ba38","blue","#00ba38","blue"))+
+  scale_shape_manual(values = c(17,21)) +
+  xlab("Time in light treatment (min)")+
+  ylab("Gray value")+
+  # ylim(20, 100)+
+  scale_x_continuous(breaks=seq(0, 60, 10))+
+  theme_bw() +
+  theme(strip.background = element_rect(colour = "black", fill = "white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.title = element_blank(),
+        axis.text=  element_text(colour="black", size=11),
+        axis.title = element_text(colour = "black", size=12),
+        aspect.ratio = 0.8,
+        legend.position="bottom", 
+        legend.box = "vertical",
+        panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+dev.off()
+
